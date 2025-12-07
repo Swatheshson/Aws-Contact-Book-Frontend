2955d9625ce1747a0f12f60a38123b8eb2f4ce6e
Swatheshson <swathesh.ags@gmail.com>
2025-12-07 20:20:23 +0530

    working in the core of service file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,30 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Component, signal } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
-  imports: [RouterOutlet],
+  standalone:true,
+  imports: [CommonModule,FormsModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'contact-app';
+export class App {
+  title = "contact-app"
+   constructor(private http:HttpClient) {}
+   
+  //initial value
+  display:boolean = false;
+  //variable to hold values 
+  name:string = ""
+  numbervariable!:bigint
+  
+  change(){
+    this.display = true;
+  }
+  add(){
+    //should pass to the backend
+  }
 }
