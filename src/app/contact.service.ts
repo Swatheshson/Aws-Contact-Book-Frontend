ffef5cca484ca711123d209590aef3c2f661b8c3
Swatheshson <swathesh.ags@gmail.com>
2025-12-10 16:34:14 +0530

    the observable part

--- src/app/contact.service.ts
+++ src/app/contact.service.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Contact } from './contact.interface';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -11,9 +12,11 @@ export class ContactService {
 
    }
 
+   private apiUrl = 'https://your-backend-api.com/contacts';
+
   //method should be here so that the ts fiel will access this method
-  addContact(acontact:Contact):Contact{
-    return acontact;
+  addContact(acontact:Contact):Observable<Contact>{
+    return this.http.post<Contact>(this.apiUrl,acontact);
   }
   
 }
